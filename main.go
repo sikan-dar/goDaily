1f95920f85102a62e5d4dbff0303a9f67ca7a367
theExicuter <alisikandar2421@gmail.com>
2024-08-07 21:07:08 +0530

    print and vars

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello Sikan-dar") //simple function to print line
+
+	//using variables
+	var firstVar string = "Artreus"
+	var secVar = "naruto"
+	var thirdVar string
+
+	fmt.Println(firstVar, secVar, thirdVar)
+
+	firstVar = "kratos"
+	thirdVar = "sakura"
+
+	fmt.Println(firstVar, secVar, thirdVar)
+}
